6fa7c6f98264ded93a647bc92158fb232107c1a7
Michael Grant <serus12345@gmail.com>
2023-03-23 19:00:38 -0400

    catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -12,7 +12,6 @@ state.cart = new Cart([]);
 // (the things in the state.allProducts array) into the drop down list.
 function populateForm() {
 
-
   // Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in state.allProducts) {
@@ -22,26 +21,21 @@ function populateForm() {
     selectElement.appendChild(optionElement);
   }
 
-
 }
-
 
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
 
-
   // Prevent the page from reloading
   event.preventDefault();
-
 
   // Do all the things ...
   addSelectedItemToCart();
   state.cart.saveToLocalStorage();
   state.cart.updateCounter();
   updateCartPreview();
-
 
 }
 
@@ -73,7 +67,6 @@ function updateCartPreview() {
 // Call that handleSubmit method above and kick off the whole process
 const catalogForm = document.getElementById('catalog');
 catalogForm.addEventListener('submit', handleSubmit);
-
 
 // Before anything else of value can happen, we need to fill in the select
 // drop down list in the form.
