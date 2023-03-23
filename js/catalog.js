985e243f21f7b31408288693f599f94df5adc017
Michael Grant <serus12345@gmail.com>
2023-03-23 17:33:48 -0400

    worked on app and catalog

--- js/catalog.js
+++ js/catalog.js
@@ -9,10 +9,13 @@ state.cart = new Cart([]);
 // (the things in the state.allProducts array) into the drop down list.
 function populateForm() {
 
-  //TODO: Add an <option> tag inside the form's select for each product
+  // Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in state.allProducts) {
-
+    const optionElement = document.createElement('option');
+    optionElement.value = state.allProducts[i].name;
+    optionElement.textContent = state.allProducts[i].name;
+    selectElement.appendChild(optionElement);
   }
 
 }
@@ -22,7 +25,8 @@ function populateForm() {
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
 
-  // TODO: Prevent the page from reloading
+  // Prevent the page from reloading
+  event.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -32,17 +36,26 @@ function handleSubmit(event) {
 
 }
 
-// TODO: Add the selected item and quantity to the cart
+// Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
-  // TODO: suss out the item picked from the select list
-  // TODO: get the quantity
-  // TODO: using those, add one item to the Cart
+  // Suss out the item picked from the select list
+  const selectedItem = document.getElementById('items').value;
+  // Get the quantity
+  const selectedQuantity = parseInt(document.getElementById('quantity').value);
+  // Using those, add one item to the Cart
+  state.cart.addItem(selectedItem, selectedQuantity);
 }
 
-// TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
+// As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
-  // TODO: Get the item and quantity from the form
-  // TODO: Add a new element to the cartContents div with that information
+  // Get the item and quantity from the form
+  const selectedItem = document.getElementById('items').value;
+  const selectedQuantity = parseInt(document.getElementById('quantity').value);
+  // Add a new element to the cartContents div with that information
+  const cartContentsElement = document.getElementById('cartContents');
+  const newCartItemElement = document.createElement('p');
+  newCartItemElement.textContent = `${selectedItem} x ${selectedQuantity}`;
+  cartContentsElement.appendChild(newCartItemElement);
 }
 
 // Set up the "submit" event listener on the form.
